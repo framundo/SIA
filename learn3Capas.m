d83267b7b2d35110675904db22a5f1854452ebbb
ConcoMB <cmaderblanco@gmail.com>
2013-04-18 18:50:41 -0300

    la papa

--- learn3Capas.m
+++ learn3Capas.m
@@ -16,28 +16,23 @@
 % limitMult: El valor a multiplicar el limite para probar si se aprendio o
 %   no (Si lmit es 500 y limitMult es 2, va a aprender los 500 primeros
 %   valores y va a generalizar los otros 500 (va a probalo con 1000 = 2*500)
-function W = learn(S, eta, func, layers, inLength, times, margin, b, adaptation, correction, momentum, calc_error, limit, limitMult)
+function W = learn3Capas(S, eta, func, layers, inLength, times, margin, b, adaptation, correction, momentum, calc_error, limit, limitMult)
     tic()
     l = length(layers);
     S = S./max(S);
-    W = cell(1,l);
-    W_old = cell(1,l);
-    g = cell(1,l);
-    g_prime = cell(1,l);
     etaPlot = [];
     etaPlot(1) = eta;
     etaIndex=2;
-    for k =1:l
-        [G, G_prime] = calculateG(func(k));
-        g{k} = G;
-        g_prime{k} = G_prime;
-    end
-    W{1} = ((rand(layers(1), inLength+1))-0.5)/2;
-    W_old{1} = zeros(layers(1), inLength+1);
-    for k=2:l
-        W{k} = ((rand(layers(k), layers(k-1)+1))-0.5)/2;
-        W_old{k} = zeros(layers(k), layers(k-1)+1);
-    end
+    g{1} = @sigmoid;
+    g{2} = @sigmoid;
+    g{3} = @sigmoid;
+    W1 = ((rand(layers(1), inLength+1))-0.5)/2;
+    W1_old = zeros(layers(1), inLength+1);
+        W2 = ((rand(layers(2), layers(1)+1))-0.5)/2;
+        W2_old = zeros(layers(2), layers(1)+1);
+        W3 = ((rand(layers(3), layers(2)+1))-0.5)/2;
+        W3_old = zeros(layers(3), layers(2)+1);
+       
     flag = 1;
 	consecutive = [0 0];
 	if(adaptation(1)==0 && adaptation(2)==0)
@@ -49,17 +44,16 @@ function W = learn(S, eta, func, layers, inLength, times, margin, b, adaptation,
     cuad = zeros(1, fix(times/limit));
     cuadindex=1;
     dif = zeros(1, times);
-    O = cell(l);
-    H = cell(l);
-    data = cell(l+1);
-    d = cell(l);
-    delta = cell(l);
+   
     
     while (flag)
         for t = 1:times
             
             % Error cuadratico medio
             if(calc_error && rem(t, limit) == 0)
+                W{1} = W1;
+                W{2} = W2;
+                W{3} = W3;
                 cuad(cuadindex) = calculateECM(cuad, S, t, W, g, layers, b, inLength, limit, 0);
                 if (cuad(cuadindex) <margin)
                     break;
@@ -74,25 +68,28 @@ function W = learn(S, eta, func, layers, inLength, times, margin, b, adaptation,
                e(k) = S(x-k);
             end
             
-            data{1} = [-1 e];
+            data1 = [-1 e];
             % ida
-            for k=1:l
-                [o, h] = calculate(W{k}, data{k}, g{k}, layers(k), b);
-                O{k} = o;
-                H{k} = h;
-                data{k+1} = [-1, O{k}];
-            end
+                [o1, h1] = calculate(W1, data1, @sigmoid, layers(1), b);
+                data2 = [-1, o1];
+                [o2, h2] = calculate(W2, data2, @sigmoid, layers(2), b);
+                data3 = [-1, o2];
+                [o3, h3] = calculate(W3, data3, @sigmoid, layers(3), b);
+            
             
             % Vuelta
             
-            dif(t) = S(x) - O{l};
-            d{l} = (g_prime{l}(H{l}, b) + correction) * dif(t);
-            delta{l} = eta * d{l} * data{l};
-            for k=l-1:-1:1
-                innerSum = W{k+1}(:, 2:end)' * d{k+1}';
-                d{k} = (correction + g_prime{k}(H{k}, b)) .* innerSum';
-                delta{k} = eta * d{k}' * data{k};
-            end
+            dif(t) = S(x) - o3;
+            d3 = (sigmoid_derivated(h3, b) + correction) * dif(t);
+            delta3 = eta * d3 * data3;
+                innerSum = W3(:, 2:end)' * d3';
+                d2 = (correction + sigmoid_derivated(h2, b)) .* innerSum';
+                delta2 = eta * d2' * data2;
+                
+                innerSum = W2(:, 2:end)' * d2';
+                d1 = (correction + sigmoid_derivated(h1, b)) .* innerSum';
+                delta1 = eta * d1' * data1;
+            
      
             %Control de aumento o disminucion de eta
             addDelta = 1;
@@ -111,7 +108,10 @@ function W = learn(S, eta, func, layers, inLength, times, margin, b, adaptation,
                     end
                 elseif (dif(t) < dif(t-1))
                     if (consecutive(1) == 0)
-                        previous_W = W;
+                         previous_W1 = W1;
+                         previous_W2 = W2;
+                         previous_W3 = W3;
+
                     end
                     consecutive(1) = consecutive(1)+1;
                     consecutive(2)=0;
@@ -121,17 +121,22 @@ function W = learn(S, eta, func, layers, inLength, times, margin, b, adaptation,
                         eta = eta+ adaptation(1);
                         etaPlot(etaIndex)=eta;
                         etaIndex = etaIndex+1;
-                        W = previous_W;
+                        W1 = previous_W1;
+                        W2 = previous_W2;
+                        W3 = previous_W3;
                     end
                 end
             end
             if (addDelta)
-                for k=1:l
-                   W{k} = W{k} + delta{k} + momentum * W_old{k};
-                end
+                   W1 = W1 + delta1 + momentum * W1_old;
+                   W2 = W2 + delta2 + momentum * W2_old;
+                   W3 = W3 + delta3 + momentum * W3_old;
             end
         end
-        W_old = W;
+        W1_old = W1;
+        W2_old = W2;
+        W3_old = W3;
+
 %         i=0;
         flag = 0;
 %         if (margin > 0)
@@ -143,6 +148,11 @@ function W = learn(S, eta, func, layers, inLength, times, margin, b, adaptation,
 %             end
 %         end
     end
+    W{1} = W1;
+    W{2} = W2;
+    W{3} = W3;
+    
+    
     i =inLength+1;
     y = zeros(1, limit - (inLength+1));
     y2 = zeros(1, limit - (inLength+1));
